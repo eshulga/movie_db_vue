455687965fab8f8085f3e36cc7ff62b010d47ce5
Евгений Шульга <ewgenij.shulga@gmail.com>
2018-04-18 00:51:05 +0300

    add sidebar

--- my-project/src/components/header.vue
+++ my-project/src/components/header.vue
@@ -2,14 +2,37 @@
     <header>
         <div class="header-top-line"></div>
         <div class="header-line">
+            <div @click="toggleSidebar()" class="sidebar-toggle" :class="{ open: toggleActive }" id="nav-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
             <h1>TOP-киношка</h1>
         </div>
+        <sidebar />
     </header>
 </template>
 
 <script>
+
+import Sidebar from './sidebar.vue'
+
 export default {
-  name: 'app-header'
+  name: 'app-header',
+  components: {
+    sidebar: Sidebar
+  },
+  data: function () {
+    return {
+      toggleActive: false
+    }
+  },
+  methods: {
+    toggleSidebar () {
+      this.toggleActive = !this.toggleActive
+      this.$emit('toggleSidebar')
+    }
+  }
 }
 </script>
 
@@ -19,12 +42,17 @@ export default {
 
     .header {
 
+        position: relative;
+        z-index: 999;
+
         &-top-line {
             min-height: 25px;
             background-color: $color-primary-dark;
         }
 
         &-line {
+            position: relative;
+            z-index: 999;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -35,5 +63,87 @@ export default {
         h1 {
             font-size: 25px;
         }
+    }
+
+    #nav-icon {
+        width: 40px;
+        height: 30px;
+        position: absolute;
+        left: 30px;
+        -webkit-transform: rotate(0deg);
+        -moz-transform: rotate(0deg);
+        -o-transform: rotate(0deg);
+        transform: rotate(0deg);
+        -webkit-transition: .5s ease-in-out;
+        -moz-transition: .5s ease-in-out;
+        -o-transition: .5s ease-in-out;
+        transition: .5s ease-in-out;
+        cursor: pointer;
+
+        span {
+            display: block;
+            position: absolute;
+            height: 6px;
+            width: 100%;
+            background: $text-color-primary;
+            border-radius: 9px;
+            opacity: 1;
+            left: 0;
+            -webkit-transform: rotate(0deg);
+            -moz-transform: rotate(0deg);
+            -o-transform: rotate(0deg);
+            transform: rotate(0deg);
+            -webkit-transition: .25s ease-in-out;
+            -moz-transition: .25s ease-in-out;
+            -o-transition: .25s ease-in-out;
+            transition: .25s ease-in-out;
+        }
+    }
+
+    #nav-icon span:nth-child(1) {
+        top: 0px;
+        -webkit-transform-origin: left center;
+        -moz-transform-origin: left center;
+        -o-transform-origin: left center;
+        transform-origin: left center;
+    }
+
+    #nav-icon span:nth-child(2) {
+        top: 15px;
+        -webkit-transform-origin: left center;
+        -moz-transform-origin: left center;
+        -o-transform-origin: left center;
+        transform-origin: left center;
+    }
+
+    #nav-icon span:nth-child(3) {
+        top: 30px;
+        -webkit-transform-origin: left center;
+        -moz-transform-origin: left center;
+        -o-transform-origin: left center;
+        transform-origin: left center;
+    }
+
+    #nav-icon.open span:nth-child(1) {
+        -webkit-transform: rotate(45deg);
+        -moz-transform: rotate(45deg);
+        -o-transform: rotate(45deg);
+        transform: rotate(45deg);
+        top: 3px;
+        left: 8px;
+    }
+
+    #nav-icon.open span:nth-child(2) {
+        width: 0%;
+        opacity: 0;
+    }
+
+    #nav-icon.open span:nth-child(3) {
+        -webkit-transform: rotate(-45deg);
+        -moz-transform: rotate(-45deg);
+        -o-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+        top: 32px;
+        left: 8px;
     }
 </style>
---
+++ my-project/src/components/sidebar.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="sidebar" :class="{ active: active }">
+    <nav>
+      <ul>
+        <li v-for="(item, index)  in items" :key="index">{{ item.title }}</li>
+          </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+
+  data: function () {
+    return {
+      active: false,
+      items: [
+        {
+          title: 'Главная',
+          route: 'home'
+        },
+        {
+          title: 'Избранное',
+          route: 'favorite'
+        }
+      ]
+    }
+  },
+
+  mounted: function () {
+    this.$parent.$on('toggleSidebar', () => {
+      this.active = !this.active
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+    .sidebar {
+        position: absolute;
+        z-index: 99;
+        background-color: #fff;
+        padding: 50px 50px;
+        min-width: 250px;
+        min-height: calc(100vh - 105px);
+        -webkit-box-shadow: 3px 1px 5px 0px rgba(0,0,0,0.35);
+        -moz-box-shadow: 3px 1px 5px 0px rgba(0,0,0,0.35);
+        box-shadow: 3px 1px 5px 0px rgba(0,0,0,0.35);
+        transition: all 0.5s;
+        transform: translate(-105%);
+
+        &.active {
+            transform: translate(0);
+        }
+
+        ul li {
+            padding: 15px 0;
+            list-style: none;
+            text-align: left;
+            text-transform: uppercase;
+            cursor: pointer;
+        }
+    }
+</style>
