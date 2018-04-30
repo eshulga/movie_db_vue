3c0a0b37b5f0cbb74605fdc6964e165f6bc88db7
Evgenii Shulga <ewgenij.shulga@gmail.com>
2018-04-30 17:47:03 +0300

    add shortCard responsive

--- src/components/ShortCard/shortCard.vue
+++ src/components/ShortCard/shortCard.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="short-card-wrapper">
       <div class="short-card__img">
-          <img :src="`https://image.tmdb.org/t/p/w185/${poster}`" alt="">
+          <!-- <img :src="`https://image.tmdb.org/t/p/w185/${poster}`" alt=""> -->
+          <picture>
+            <source :srcset="`https://image.tmdb.org/t/p/w700_and_h392_bestv2/${poster}`" media="(max-width: 480px)">
+            <img :src="`https://image.tmdb.org/t/p/w185/${poster}`">
+          </picture>
       </div>
-      <div class="short-card__description">
-        <h2>{{title}}</h2>
-        <p>{{overview}}</p>
-        <router-link :to="{ 'name': 'movie', 'params': { 'id': id } }">Подробнее</router-link>
+      <div class="short-card__info">
+        <h2 class="short-card__title">{{title}}</h2>
+        <p class="short-card__description">{{overview}}</p>
+        <div class="short-card__controls">
+          <router-link class="short-card__link" :to="{ 'name': 'movie', 'params': { 'id': id } }">Подробнее</router-link>
+        </div>
       </div>
   </div>
 </template>
